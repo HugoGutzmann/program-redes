3de3a5c677644f0f54ab35378edcee76319970c7
HugoGutzmann <facahugo@gmail.com>
2018-07-03 16:48:03 -0300

    Add files via upload

---
+++ trabprogramacao/controler/a.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 03/07/18
+ * Time: 16:43
+ */
+
+class a
+{
+
+}
---
+++ trabprogramacao/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="row">
+
+    <input type="text">.
+    <input type="text">.
+    <input type="text">.
+    <input type="text">/
+    <input type="decimal">
+    <input type="submit">
+
+</div>
+
+</body>
+</html>
